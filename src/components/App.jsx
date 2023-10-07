5c4010a4e08543062f62709f5f807bb66fde34df
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2023-10-07 13:58:08 -0400

    q

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import React from "react";
+import Comp from "./Comp";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+
+    <Comp />
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
---
+++ src/components/Comp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function comp() {
+  return (
+    <div>
+        
+        <h1>HELLO</h1>
+
+    </div>
+  )
+};
